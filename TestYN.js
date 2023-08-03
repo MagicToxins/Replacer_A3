3d4de295f25f57876c12e7a1aff767dde63a7c93
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-02 20:39:02 -0400

    Create TestYN.js

---
+++ TestYN.js
@@ -0,0 +1,1 @@
+Javascript:void function(){let b=RegExp(/[\[\(]?Y\/N(?!\/n)[\]\)]?/,"ig"),c=Ain,a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,c);document.getElementById("workskin").innerHTML=a}();
