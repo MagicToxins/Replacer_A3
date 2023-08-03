f48b558cac865f27da0decd1f31c1af16bad4c93
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-03 12:15:54 -0400

    Create SNReplacer.js

---
+++ SNReplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?S\/N(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Plague");document.getElementById("workskin").innerHTML=a}();
