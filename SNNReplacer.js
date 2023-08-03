d5eee028f96749a6c8d31ff658b07afcd63f636d
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-03 18:26:51 -0400

    Create SNNReplacer.js

---
+++ SNNReplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?S\/N\/N(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Doc");document.getElementById("workskin").innerHTML=a}();
