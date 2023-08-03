3583cbdca5232669e9d60559c7193a468de605f3
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-02 21:48:13 -0400

    Update TestYN.js

--- TestYN.js
+++ TestYN.js
@@ -1,1 +1,1 @@
-Javascript:void function(){let a=document.getElementById("workskin").innerHTML;a=a.replaceAll(/[\[\(]?Y\/N(?!\/n)[\]\)]?/, "Ain");document.getElementById("workskin").innerHTML=a}();
+Javascript:void function(){let b=RegExp(/[\[\(]?Y\/N(?!\/n)[\]\)]?/,"g"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,Ain);document.getElementById("workskin").innerHTML=a}();
