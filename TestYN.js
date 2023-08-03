6f76ae58e8edc4b232564f134877ece941504261
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-02 21:15:21 -0400

    Update TestYN.js

--- TestYN.js
+++ TestYN.js
@@ -1,1 +1,1 @@
-Javascript:void function(){document.getElementById("workskin").innerHTML = document.body.innerHTML.replace(/[\[\(]?Y\/N(?!\/n)[\]\)]?/, Ain)}();
+Javascript:void function(){let document.getElementById("workskin").innerHTML = document.body.innerHTML.replace(/[\[\(]?Y\/N(?!\/n)[\]\)]?/, Ain)}();
