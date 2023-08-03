0c1f31795f89db3222090df7c8845bf7485c9b76
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-02 21:42:27 -0400

    Update TestYN.js

--- TestYN.js
+++ TestYN.js
@@ -1,1 +1,1 @@
-Javascript: function(){let b=String(/[\[\(]?L\/N(?!\/n)[\]\)]?/),c=String(Ain),a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,c);document.getElementById("workskin").innerHTML=a}();
+Javascript:void function replaceAll(/[\[\(]?Y\/N(?!\/n)[\]\)]?/, "Ain");
