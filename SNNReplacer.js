459997a14b0c1255001f11120da85451ff96060a
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-03 18:26:15 -0400

    Delete SNNReplacer.js

--- SNNReplacer.js
+++
@@ -1,1 +0,0 @@
-void function(){let b=RegExp(/[\[\(]?S\/N\/N(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Crow");document.getElementById("workskin").innerHTML=a}();
