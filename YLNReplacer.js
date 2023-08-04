b5996edaad331679fd2f4899f98892ffa94266a3
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-03 22:08:49 -0400

    Create YLNReplacer.js

---
+++ YLNReplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?Y\/L\/N(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Black");document.getElementById("workskin").innerHTML=a}();
