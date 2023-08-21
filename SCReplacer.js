e0a5269672f6601c76d23ac29dc18b5bdfa101ea
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-21 18:04:50 -0400

    Create SCReplacer.js

---
+++ SCReplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?S\/C(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"pale");document.getElementById("workskin").innerHTML=a}();
