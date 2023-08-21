864a82263dc30b3a4f96d327c3d3c479544ae05d
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-08-21 18:01:39 -0400

    Create HLReplacer.js

---
+++ HLReplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?H\/L(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"long");document.getElementById("workskin").innerHTML=a}();
