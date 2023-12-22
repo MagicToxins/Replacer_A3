1d33e39bc521467d7a05fbc63877755bc99be204
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-12-22 11:35:00 -0500

    Create HairReplacer.js

---
+++ HairReplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?(hair length)(?!\/n)[\]\)]?/,"g"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"long");document.getElementById("workskin").innerHTML=a}();
