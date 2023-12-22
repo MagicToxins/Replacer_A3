443c36e6c265f12428dc8afa6de5803624e98846
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-12-22 11:03:58 -0500

    Update and rename NameReplacer4.js to NameReplacer5.js

---
+++ NameReplacer5.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?(Name)(?!\/n)[\]\)]?/,"g"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Ain");document.getElementById("workskin").innerHTML=a}();
