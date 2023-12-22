74f9bfb8e3f15153a9a5b7d3e484555dd834d683
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2023-12-22 10:57:54 -0500

    Update and rename NameReplacer2.js to NameReplacer3.js

--- NameReplacer2.js
+++
@@ -1,1 +0,0 @@
-void function(){let b=RegExp(/[\[\(]?(Name)(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Ain");document.getElementById("workskin").innerHTML=a}();
---
+++ NameReplacer3.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/?(Name)?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Ain");document.getElementById("workskin").innerHTML=a}();
