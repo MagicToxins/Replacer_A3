3bee78bb134f1df2161d38c642f9aa03a03c15b4
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2024-11-25 14:36:50 -0500

    Create YAreplacer.js

---
+++ YAreplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?Y\/A(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"26");document.getElementById("workskin").innerHTML=a}();
