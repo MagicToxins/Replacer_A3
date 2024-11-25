28c68b924f1ca4d741af17a6953878906ae45eb4
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2024-11-25 14:38:14 -0500

    Create YGreplacer.js

---
+++ YGreplacer.js
@@ -0,0 +1,1 @@
+void function(){let b=RegExp(/[\[\(]?Y\/G(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"non-binary");document.getElementById("workskin").innerHTML=a}();
