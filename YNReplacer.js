adae9affcf403882a7e83e74f925feafb516f42e
MagicToxins <141285973+MagicToxins@users.noreply.github.com>
2024-11-25 14:41:36 -0500

    Update YNReplacer.js

--- YNReplacer.js
+++ YNReplacer.js
@@ -1,1 +1,1 @@
-void function(){let b=RegExp(/[\[\(]?Y\/N(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Ain");document.getElementById("workskin").innerHTML=a}();
+void function(){let b=RegExp(/[\[\(]?Y\/N(?!\/n)[\]\)]?/,"ig"), a=document.getElementById("workskin").innerHTML;a=a.replaceAll(b,"Nim");document.getElementById("workskin").innerHTML=a}();
